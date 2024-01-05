466e3e98cfa1e20e41bbd8a3900ca0d7060aa0e7
nowislewis <41566630+nowislewis@users.noreply.github.com>
2024-01-05 09:48:27 +0800

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,24 +1,30 @@
+if status is-interactive
+
+end
+
+fish_config theme choose "Dracula"
 # style
 starship init fish | source
 
-set -x PATH $PATH $HOME/.local/bin
+set -gx PATH $PATH $HOME/.local/bin
 # gnu sort behavior
-set -x LC_COLLATE POSIX
+set -gx LC_COLLATE POSIX
 
-set host_ip 127.0.0.1
+set -gx host_ip 127.0.0.1
 function proxy_on
-	set -x http_proxy http://$host_ip:7890
-	set -x https_proxy http://$host_ip:7890
+	set -gx http_proxy "http://$host_ip:7890"
+	set -gx https_proxy "http://$host_ip:7890"
 	echo -e "终端代理已开启。"
 end
 
 function proxy_off
-	set -e http_proxy
-  set -e https_proxy
-  set -e all_proxy
-	echo -e "终端代理已关闭。"
+    set -e http_proxy
+    set -e https_proxy
+    set -e all_proxy
+    echo -e "终端代理已关闭。"
 end
 
-proxy_on
-
-alias emacs 'emacsclient -c|| emacs'
+function e
+    emacsclient -c $$argv||emacs $argv
+end
+pyenv init -| source
