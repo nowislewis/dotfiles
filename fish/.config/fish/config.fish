aa6dc5252d7a9f7eb4ac9a00f4f9f01d470281ba
lewisliu <1226228799@qq.com>
2024-01-07 11:08:05 +0800

    add homebrew mirror in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,10 +6,22 @@ fish_config theme choose "Dracula"
 # style
 starship init fish | source
 
+
+# var
+if test (uname) = "Darwin"
+   set -xg HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
+   set -xg HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
+   set -xg HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
+   set -xg HOMEBREW_API_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles/api"
+end
+
+
 set -gx PATH $PATH $HOME/.local/bin
 # gnu sort behavior
 set -gx LC_COLLATE POSIX
 
+pyenv init -| source
+# proxy
 set -gx host_ip 127.0.0.1
 function proxy_on
 	set -gx http_proxy "http://$host_ip:7890"
@@ -27,4 +39,5 @@ end
 function e
     emacsclient -c $$argv||emacs $argv
 end
-pyenv init -| source
+
+
