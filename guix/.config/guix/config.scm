5e07f0c523e94873cb40f9c6edd8f2d01e2fc108
lewisliu <1226228799@qq.com>
2024-05-02 15:21:30 +0800

    enable bluetooth in guix, enable fish in zsh

--- guix/.config/guix/config.scm
+++ guix/.config/guix/config.scm
@@ -10,13 +10,14 @@
 ;; Indicate which modules to import to access the variables
 ;; used in this configuration.
 (use-modules (gnu) (nongnu packages linux))
+(use-modules (gnu) (nongnu system linux-initrd))
 (use-modules (gnu packages shells))
-(use-modules (gnu packages vim))
 (use-service-modules cups desktop networking ssh xorg sddm)
 
 (operating-system
   (kernel linux)
   (firmware (list linux-firmware))
+  (initrd microcode-initrd)
   (locale "en_US.utf8")
   (timezone "Asia/Shanghai")
   (keyboard-layout (keyboard-layout "us"))
@@ -24,52 +25,74 @@
 
   ;; The list of user accounts ('root' is implicit).
   (users (cons* (user-account
-                  (name "lewisliu")
-                  (comment "Lewisliu")
-                  (group "users")
-                  (shell (file-append zsh "/bin/zsh"))
-                  (home-directory "/home/lewisliu")
-                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                 (name "lewisliu")
+                 (comment "Lewisliu")
+                 (group "users")
+                 (shell (file-append zsh "/bin/zsh"))
+                 (home-directory "/home/lewisliu")
+                 (supplementary-groups '("wheel" ;; sudo
+                                         "netdev" ;; network devices
+                                         "lp"       ;; control bluetooth devices
+                                         "audio"    ;; control audio devices
+                                         "video"))) ;; control video devices
                 %base-user-accounts))
 
   ;; Packages installed system-wide.  Users can also install packages
   ;; under their own account: use 'guix search KEYWORD' to search
   ;; for packages and 'guix install PACKAGE' to install a package.
-  (packages (append (list vim)
+  (packages (append (map specification->package
+                         '("git"
+                           "stow"
+                           "vim"
+                           "bluez"
+                           "bluez-alsa"
+                           "blueman"
+                           ))
                     %base-packages))
 
   ;; Below is the list of system services.  To search for available
   ;; services, run 'guix system search KEYWORD' in a terminal.
+  ;; (services (append
+  ;;            (modify-services %base-services
+  ;;                             (delete login-service-type)
+  ;;                             (delete mingetty-service-type)
+  ;;                             (delete console-font-service-type)
+  ;;                             )
+  ;;            (list)
+  ;;            ))
   (services
    (modify-services
-       (append (list
-		        ;; To configure OpenSSH, pass an 'openssh-configuration'
-		        ;; record as a second argument to 'service' below.
-		        (service openssh-service-type)
-		        ;; (set-xorg-configuration
-		        ;;  (xorg-configuration (keyboard-layout keyboard-layout)))
-                (service sddm-service-type)
-                )
-	           ;; %desktop-services
-               (modify-services %desktop-services
-                 (delete gdm-service-type))
-               )
-     (guix-service-type config => (guix-configuration
-                                   (inherit config)
-                                   (substitute-urls
-                                    (append (list "https://mirror.sjtu.edu.cn/guix/" "https://ci.guix.gnu.org" "https://substitutes.nonguix.org")
-                                            %default-substitute-urls)
-                                    )
-                                   (authorized-keys
-                                    (append (list (plain-file "non-guix.pub"
-  							                                  "(public-key
+    (append (list
+		     ;; To configure OpenSSH, pass an 'openssh-configuration'
+		     ;; record as a second argument to 'service' below.
+		     (service openssh-service-type)
+		     ;; (set-xorg-configuration
+		     ;;  (xorg-configuration (keyboard-layout keyboard-layout)))
+             (service sddm-service-type)
+             (service bluetooth-service-type
+                      (bluetooth-configuration
+                       (auto-enable? #t)))
+             )
+	        ;; %desktop-services
+            (modify-services %desktop-services
+                             (delete gdm-service-type))
+            )
+    (guix-service-type config => (guix-configuration
+                                  (inherit config)
+                                  (substitute-urls
+                                   (append (list "https://mirror.sjtu.edu.cn/guix/" "https://ci.guix.gnu.org" "https://substitutes.nonguix.org")
+                                           %default-substitute-urls)
+                                   )
+                                  (authorized-keys
+                                   (append (list (plain-file "non-guix.pub"
+  							                                 "(public-key
                                     (ecc
                                      (curve Ed25519)
                                      (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)
                                     )
                                   )"
-                                                              )
-                                                  )
+                                                             )
+                                                 )
                                             %default-authorized-guix-keys))))))
   (bootloader (bootloader-configuration
                (bootloader grub-efi-bootloader)
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -10,3 +10,5 @@ then
     command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 fi
+
+fish
